1c76f58baf3c3ea8630fe7c832888f89155e47a7
iwilllearnquran <i.will.learn.quran.786@gmail.com>
2025-09-05 13:42:35 +0530

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,37 @@
+// App.js
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
-export default function HomeScreen() {
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { paddingBottom: insets.bottom }]}>
       <WebView
         source={{ uri: 'https://myquranquest.com/' }}
         style={{ flex: 1 }}
         javaScriptEnabled
         domStorageEnabled
+        overScrollMode="never"   // Android - prevent overscroll
+        bounces={false}          // iOS - prevent bounce
       />
     </SafeAreaView>
   );
 }
 
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#fff'
   }
 });
